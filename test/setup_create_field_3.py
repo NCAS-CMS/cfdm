89950adad86249c1a7dcf82e6d6ef1a11354df27
David Hassell <d.c.hassell@reading.ac.uk>
2018-04-25 11:38:13 +0100

    dev

---
+++ test/setup_create_field_3.py
@@ -0,0 +1,261 @@
+import datetime
+import os
+import sys
+import unittest
+
+import numpy
+
+import cfdm
+
+class create_fieldTest(unittest.TestCase):
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'test_file.nc')
+
+    def test_create_field(self):
+
+        # Dimension coordinates
+        dim1 = cfdm.DimensionCoordinate(data=cfdm.Data(numpy.arange(10.)))
+        dim1.set_property('standard_name', 'grid_latitude')
+        dim1.set_property('units', 'degrees')
+
+        data = numpy.arange(9.) + 20
+        data[-1] = 34
+        dim0 = cfdm.DimensionCoordinate(data=cfdm.Data(data))
+        dim0.set_property('standard_name', 'grid_longitude')
+        dim0.set_property('units', 'degrees')
+
+        array = dim0.get_array()
+
+        array = numpy.array([array-0.5, array+0.5]).transpose((1,0))
+        array[-2, 1] = 30
+        array[-1, :] = [30, 36]
+        dim0.set_bounds(cfdm.Bounds(data=cfdm.Data(array)))
+        
+        dim2 = cfdm.DimensionCoordinate(data=cfdm.Data([1.5]),
+                                        bounds=cfdm.Bounds(data=cfdm.Data([[1, 2.]])))
+        dim2.set_property('standard_name'         , 'atmosphere_hybrid_height_coordinate')
+        dim2.set_property('computed_standard_name', 'altitude')
+                      
+        dim3 = cfdm.DimensionCoordinate(data=cfdm.Data(numpy.array([15.0])))
+        dim3.set_property('standard_name', 'time')
+        dim3.set_property('units', 'days since 2004-6-1')
+        dim3.set_bounds(cfdm.Bounds(data=cfdm.Data([[0, 30.]])))
+        dim3.set_extent_parameter('climatology', True)
+        
+        # Auxiliary coordinates
+        ak = cfdm.DomainAncillary(data=cfdm.Data([10.])) #, 'm'))
+        ak.set_property('units', 'm')
+        ak.set_bounds(cfdm.Bounds(data=cfdm.Data([[5, 15.]]))) # , ak.Units)
+        
+        bk = cfdm.DomainAncillary(data=cfdm.Data([20.]))
+        bk.set_bounds(cfdm.Bounds(data=cfdm.Data([[14, 26.]])))
+        
+        aux2 = cfdm.AuxiliaryCoordinate(
+            data=cfdm.Data(numpy.arange(-45, 45, dtype='int32').reshape(10, 9)))
+        aux2.set_property('units', 'degree_N')
+        aux2.set_property('standard_name', 'latitude')
+        
+        aux3 = cfdm.AuxiliaryCoordinate(
+            data=cfdm.Data(numpy.arange(60, 150, dtype='int32').reshape(9, 10)))
+        aux3.set_property('standard_name', 'longitude')
+        aux3.set_property('units', 'degreeE')
+
+        array = numpy.ma.array(['alpha','beta','gamma','delta','epsilon',
+                               'zeta','eta','theta','iota','kappa'])
+        array[0] = numpy.ma.masked
+        aux4 = cfdm.AuxiliaryCoordinate(data=cfdm.Data(array))
+        aux4.set_property('standard_name', 'greek_letters')
+
+        # Cell measures
+        msr0 = cfdm.CellMeasure(
+            data=cfdm.Data(1+numpy.arange(90.).reshape(9, 10)*1234))
+        msr0.set_measure('area')
+        msr0.set_property('units', 'km2')
+        
+        # Data          
+        data = cfdm.Data(numpy.arange(90.).reshape(10, 9))
+
+        properties = {'units': 'm s-1'}
+        
+        f = cfdm.Field(properties=properties)
+        f.set_property('standard_name', 'eastward_wind')
+
+        axisX = f.set_domain_axis(cfdm.DomainAxis(9))
+        axisY = f.set_domain_axis(cfdm.DomainAxis(10))
+        axisZ = f.set_domain_axis(cfdm.DomainAxis(1))
+        axisT = f.set_domain_axis(cfdm.DomainAxis(1))
+
+        f.set_data(data, axes=[axisY, axisX])
+        
+        x = f.set_dimension_coordinate(dim0, axes=[axisX])
+        y = f.set_dimension_coordinate(dim1, axes=[axisY])
+        z = f.set_dimension_coordinate(dim2, axes=[axisZ])
+        t = f.set_dimension_coordinate(dim3, axes=[axisT])
+
+        lat   = f.set_auxiliary_coordinate(aux2, axes=[axisY, axisX])
+        lon   = f.set_auxiliary_coordinate(aux3, axes=[axisX, axisY])
+        greek = f.set_auxiliary_coordinate(aux4, axes=[axisY])
+
+        ak = f.set_domain_ancillary(ak, axes=[axisZ])
+        bk = f.set_domain_ancillary(bk, axes=[axisZ])
+
+        # Coordinate references
+        ref0 = cfdm.CoordinateReference(
+            parameters={'grid_mapping_name': 'rotated_latitude_longitude',
+                        'grid_north_pole_latitude': 38.0,
+                        'grid_north_pole_longitude': 190.0,
+                        'earth_radius': 6371007,},
+            coordinates=[x, y, lat, lon]
+        )
+
+        f.set_cell_measure(msr0, axes=[axisX, axisY])
+
+        f.set_coordinate_reference(ref0)
+
+        orog = cfdm.DomainAncillary(data=f.get_data())
+        orog.set_property('standard_name', 'surface_altitude')
+        orog.set_property('units', 'm')
+        orog = f.set_domain_ancillary(orog, axes=[axisY, axisX])
+
+        
+        ref1 = cfdm.CoordinateReference(
+            parameters={'standard_name': 'atmosphere_hybrid_height_coordinate',
+                        'computed_standard_name': 'altitude',
+                        'earth_radius' : 6371007,},
+            domain_ancillaries={'orog': orog,
+                                'a'   : ak,
+                                'b'   : bk},
+            coordinates=[z]
+        )
+        
+        ref1 = f.set_coordinate_reference(ref1)
+
+        # Field ancillary variables
+#        g = f.transpose([1, 0])
+        g = f.copy()
+#        g.standard_name = 'ancillary0'
+#        g *= 0.01
+        anc = cfdm.FieldAncillary(data=g.get_data())
+        anc.standard_name = 'ancillaryA'
+        f.set_field_ancillary(anc, axes=[axisY, axisX])
+        
+        g = f[0]
+        g.squeeze(copy=False)
+#        g.standard_name = 'ancillary2'
+#        g *= 0.001
+        anc = cfdm.FieldAncillary(data=g.get_data())
+        anc.standard_name = 'ancillaryB'
+        f.set_field_ancillary(anc, axes=[axisX])
+
+        g = f[..., 0]
+        g = g.squeeze()
+#        g.standard_name = 'ancillary3'
+#        g *= 0.001
+        anc = cfdm.FieldAncillary(data=g.get_data())
+        anc.standard_name = 'ancillaryC'
+        f.set_field_ancillary(anc, axes=[axisY])
+
+        
+        f.set_property('flag_values', numpy.array([1, 2, 4], 'int32'))
+        f.set_property('flag_meanings', 'a bb ccc')
+        f.set_property('flag_masks', [2, 1, 0])
+
+        cm0 =  cfdm.CellMethod(axes=[axisX],
+                               properties={'method'   : 'mean',
+                                           'intervals': [cfdm.Data(1, 'day')],
+                                           'comment'  : 'ok'})
+    
+        cm1 =  cfdm.CellMethod(axes=[axisY],
+                               properties={'method': 'maximum',
+                                           'where' : 'sea'})
+
+
+        
+        f.set_cell_method(cm0)
+        f.set_cell_method(cm1)
+
+#        for cm in cfdm.CellMethod.parse(axisX+': mean (interval: 1 day comment:# ok) '+axisY+': max where sea'):
+#            f.set_cell_method(cm)
+
+        print repr(f)
+        print f
+        print f.constructs()
+        print f.construct_axes()
+        
+        f.dump()
+        print "####################################################"
+        cfdm.write(f, self.filename, fmt='NETCDF3_CLASSIC',_debug=True)
+#        f.dump()
+
+        g = cfdm.read(self.filename, _debug=True) #, squeeze=True)
+        for x in g:
+            x.print_read_report()
+
+        self.assertTrue(len(g) == 1, '{} != 1'.format(len(g)))
+
+        g = g[0].squeeze(copy=False)
+        
+#        g.dump()
+#        print g
+        self.assertTrue(sorted(f.constructs()) == sorted(g.constructs()),
+                        '\n\nf (created in memory)\n{}\n\n{}\n\ng (read from disk)\n{}\n\n{}'.format(
+                            sorted(f.constructs()),
+                            sorted(f.constructs().items()),
+                            sorted(g.constructs()),
+                            sorted(g.constructs().items())))
+
+        self.assertTrue(f.equals(f.copy(), traceback=True),
+                        "Field f not equal to a copy of itself")
+        print 2
+        self.assertTrue(g.equals(g.copy(), traceback=True),
+                        "Field g not equal to a copy of itself")
+
+#        print'f'
+#        print f
+#        print 'g'
+#        print g
+#        f.dump()
+#        g.dump()
+
+        print 3
+#        g.dump()
+#        f.dump()
+        self.assertTrue(g.equals(f, traceback=True),
+                        "Field not equal to itself read back in")
+
+        
+        x = g.dump(display=False)
+        x = f.dump(display=False)
+
+        g = cfdm.read(self.filename, _debug=True, field=['domain_ancillary'])
+        for x in g:
+            x.print_read_report()
+
+
+        print g
+#        for x in g:
+#            x.dump()
+#        h = g.field('domainancillary2')
+#        h.dump()
+#        print h
+#        
+#
+#        h = g.field('domainancillary1')
+#        print h
+#        
+#        h = g.field('domainancillary0')
+#        print h
+#        
+#        h = g.field('cellmeasure0')
+#        print h
+
+        
+    #--- End: def
+
+#--- End: class
+
+if __name__ == "__main__":
+    print 'Run date:', datetime.datetime.now()
+    cfdm.environment()
+    print ''
+    unittest.main(verbosity=2)
