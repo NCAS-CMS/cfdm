8a64842f034ebe437beb4c720fd7f6b165406247
David Hassell <d.c.hassell@reading.ac.uk>
2018-05-10 10:28:28 +0100

    dev

---
+++ cfdm/structure/boundsancillary.py
@@ -0,0 +1,11 @@
+import abc
+
+import abstract
+
+
+class BoundsAncillary(abstract.PropertiesData):
+    '''Coordinate bounds ancillary array
+
+    '''
+    __metaclass__ = abc.ABCMeta
+#--- End: class
---
+++ cfdm/structure/boundsmapping.py
@@ -0,0 +1,12 @@
+import abc
+
+import abstract
+
+
+class BoundsMapping(abstract.Terms):
+    '''A coordinate cell extents definition.
+
+    '''
+    __metaclass__ = abc.ABCMeta
+    
+#--- End: class
