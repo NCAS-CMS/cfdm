6a2414cf0ddf9f11928d147901ee7e91e38ee767
David Hassell <d.c.hassell@reading.ac.uk>
2018-05-21 13:48:16 +0100

    dev

---
+++ cfdm/structure/coordinateancillary.py
@@ -0,0 +1,12 @@
+import abc
+
+import abstract
+
+
+class CoordinateAncillary(abstract.PropertiesData):
+    '''Coordinate ancillary array.
+
+    '''
+    __metaclass__ = abc.ABCMeta
+
+#--- End: class
