4a9fdb73c825ad040185f9aa076d5e6ff0ba0c4c
cfpython <d.c.hassell@reading.ac.uk>
2018-09-14 15:52:01 +0100

    dev

--- cfdm/abstract/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-'''
-from .container                   import Container
-from .coordinate                  import Coordinate
-from .properties                  import Properties
-from .propertiesdata              import PropertiesData
-from .propertiesdatabounds        import PropertiesDataBounds
-from .parameters                  import Parameters
-from .parametersdomainancillaries import ParametersDomainAncillaries
