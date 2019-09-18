422f1b3cdc865c809112a2098cc4ca1a5842f732
cfpython <d.c.hassell@reading.ac.uk>
2019-09-18 14:12:21 +0100

    bounds ncdimension

--- cfdm/bounds.py
+++ cfdm/bounds.py
@@ -33,6 +33,11 @@ The netCDF variable name of the bounds may be accessed with the
 `nc_set_variable`, `nc_get_variable`, `nc_del_variable` and
 `nc_has_variable` methods.
 
+The name of the trailing netCDF dimension spanned by bounds (which
+does not correspond to a domain axis construct) may be accessed with
+the `nc_set_dimension`, `nc_get_dimension`, `nc_del_dimension and
+`nc_has_dimension` methods.
+
 .. versionadded:: 1.7.0
 
     '''
