899a5c06afce22711025b1903db6776e17c85d7c
davidhassell <david.hassell@ncas.ac.uk>
2020-01-21 13:50:21 +0000

    tidy

--- cfdm/list.py
+++ cfdm/list.py
@@ -9,8 +9,6 @@ class List(mixin.NetCDFVariable,
            core.abstract.PropertiesData):
     '''A list variable required to uncompress a gathered array.
 
-TESET
-
     Compression by gathering combines axes of a multidimensional array
     into a new, discrete axis whilst omitting the missing values and
     thus reducing the number of values that need to be stored.
