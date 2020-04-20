bb016a878a329068c6fccf0409ca921007d4a2d9
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-04-20 17:53:41 +0100

    Towards #25 portable fix (sed -i -> sed + overwrite)

--- cfdm/test/test_read_write.py
+++ cfdm/test/test_read_write.py
@@ -16,6 +16,7 @@ warnings = False
 
 tmpfile  = tempfile.mktemp('.cfdm_test')
 tmpfileh  = tempfile.mktemp('.cfdm_test')
+tmpfileh2 = tempfile.mktemp('.cfdm_test')
 tmpfilec  = tempfile.mktemp('.cfdm_test')
 tmpfile0  = tempfile.mktemp('.cfdm_test')
 tmpfile1  = tempfile.mktemp('.cfdm_test')
@@ -211,22 +212,23 @@ class read_writeTest(unittest.TestCase):
 
         with self.assertRaises(OSError):
             x = cfdm.read('test_read_write.py')
-            
-        subprocess.run(' '.join(['sed', '-i', r'"1 i\ \ "', tmpfileh]),
-                       shell=True, check=True)
-        h = cfdm.read(tmpfileh)[0]
-        
-        subprocess.run(' '.join(['sed', '-i', r'"1 i\// comment"', tmpfileh]),
-                       shell=True, check=True)
-        h = cfdm.read(tmpfileh)[0]
 
-        subprocess.run(' '.join(['sed', '-i', r'"1 i\ // comment"', tmpfileh]),
-                       shell=True, check=True)
-        h = cfdm.read(tmpfileh)[0]
-
-        subprocess.run(' '.join(['sed', '-i', r'"1 i\ \t// comment"', tmpfileh]),
-                       shell=True, check=True)
-        h = cfdm.read(tmpfileh)[0]
+        for regex in [
+            r'"1 i\ \ "',
+            r'"1 i\// comment"',
+            r'"1 i\ // comment"',
+            r'"1 i\ \t// comment"'
+        ]:
+            # Note that really we just want to do an in-place sed ('sed -i')
+            # but because of subtle differences between the GNU (Linux OS) and
+            # BSD (some Mac OS) command variants a safe portable one-liner may
+            # not be possible. This will do, overwriting the intermediate file:
+            subprocess.run(
+                ' '.join(['sed', '-e', regex, tmpfileh, '>' + tmpfileh2, '&&',
+                          'mv', tmpfileh2, tmpfileh]),
+                shell=True, check=True
+            )
+            h = cfdm.read(tmpfileh)[0]
 
 #        subprocess.run(' '.join(['head', tmpfileh]),  shell=True, check=True)
 
