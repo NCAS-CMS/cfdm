2028892dde29c6e1fbfe9c85002811943fa32523
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-07-08 22:41:55 +0100

    CDL read: improve error message for invalid CDL input

--- cfdm/read_write/netcdf/netcdfread.py
+++ cfdm/read_write/netcdf/netcdfread.py
@@ -421,7 +421,21 @@ class NetCDFRead(IORead):
         # ----------------------------------------------------------------
         _cached_temporary_files[tmpfile] = x
 
-        subprocess.run(["ncgen", "-knc4", "-o", tmpfile, filename], check=True)
+        try:
+            subprocess.run(
+                ["ncgen", "-knc4", "-o", tmpfile, filename], check=True
+            )
+        except subprocess.CalledProcessError as error:
+            msg = str(error)
+            if msg.startswith(
+                "Command '['ncgen', '-knc4', '-o'"
+            ) and msg.endswith("returned non-zero exit status 1."):
+                raise ValueError(
+                    "The CDL provided is invalid so cannot be converted "
+                    "to netCDF."
+                )
+            else:
+                raise
 
         return tmpfile
 
