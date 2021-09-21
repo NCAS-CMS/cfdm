14cf5b9c7a5bbce55025e10e6af7ae1cbea21342
David Hassell <david.hassell@ncas.ac.uk>
2021-09-21 14:39:23 +0100

    updated functionality description

--- setup.py
+++ setup.py
@@ -66,16 +66,17 @@ inspecting it:
 
 The **cfdm** package can
 
-* read field constructs from netCDF and CDL datasets,
-* create new field constructs in memory,
-* write and append field constructs to netCDF datasets on disk,
+* read field and domain constructs from netCDF and CDL datasets,
+* create new field and domain constructs in memory,
+* write and append field and domain constructs to netCDF datasets on disk,
 * read, write, and create coordinates defined by geometry cells,
 * read and write netCDF4 string data-type variables,
-* read, write, and create datasets containing hierarchical groups,
-* inspect field constructs,
-* test whether two field constructs are the same,
-* modify field construct metadata and data,
-* create subspaces of field constructs,
+* read, write, and create netCDF and CDL datasets containing
+  hierarchical groups,
+* inspect field and domain constructs,
+* test whether two constructs are the same,
+* modify field and domain construct metadata and data,
+* create subspaces of field and domain constructs,
 * incorporate, and create, metadata stored in external files, and
 * read, write, and create data that have been compressed by convention
   (i.e. ragged or gathered arrays), whilst presenting a view of the
