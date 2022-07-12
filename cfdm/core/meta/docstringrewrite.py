711c008bac15710649653edd997ee8be48f05fef
David Hassell <david.hassell@ncas.ac.uk>
2022-07-12 08:49:23 +0100

    Python 3.10 __wrapper__

--- cfdm/core/meta/docstringrewrite.py
+++ cfdm/core/meta/docstringrewrite.py
@@ -197,10 +197,10 @@ class DocstringRewriteMeta(type):
             if is_classmethod:
                 attrs[attr_name] = classmethod(attr)
 
-            if is_staticmethod:
+            elif is_staticmethod:
                 attrs[attr_name] = staticmethod(attr)
 
-            if is_wrapped:
+            elif is_wrapped:
                 wrapper.__doc__ = attr.__doc__
                 wrapper.__wrapped__ = attr
                 attrs[attr_name] = wrapper
