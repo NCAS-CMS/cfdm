c476f2a0e8eefa6697f470d9ba0ebf7755bdfe45
David Hassell <davidhassell@users.noreply.github.com>
2022-10-03 18:01:14 +0100

    Typo
    Co-authored-by: Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>

--- cfdm/data/data.py
+++ cfdm/data/data.py
@@ -1653,7 +1653,7 @@ class Data(Container, NetCDFHDF5, core.Data):
         .. versionadded:: (cfdm) 1.10.0.1
 
         .. seealso:: `get_dependent_tie_points`,
-                     `get_ti_point_indices`, `get_index`, `get_index`,
+                     `get_tie_point_indices`, `get_index`,
                      `get_list`
 
         :Parameters:
