18022e21d40db13de7fd5cfcd7826a8bbbc97472
David Hassell <david.hassell@ncas.ac.uk>
2025-02-19 10:44:26 +0000

    dev

--- cfdm/__init__.py
+++ cfdm/__init__.py
@@ -54,6 +54,7 @@ _requires = core._requires + (
     "scipy",
     "h5netcdf",
     "s3fs",
+    "uritools",
 )
 
 _error0 = f"cfdm requires the modules {', '.join(_requires)}. "
@@ -150,6 +151,18 @@ else:
             "Bad dask version: cfdm requires "
             f"{_minimum_vn}<=dask<={_maximum_vn}. "
             f"Got {_dask_version} at {dask.__file__}"
+        )
+
+try:
+    import uritools
+except ImportError as error1:
+    raise ImportError(_error0 + str(error1))
+else:
+    _minimum_vn = "4.0.3"
+    if Version(uritools.__version__) < Version(_minimum_vn):
+        raise ValueError(
+            f"Bad uritools version: cfdm requires uritools>={_minimum_vn}. "
+            f"Got {uritools.__version__} at {uritools.__file__}"
         )
 
 del _minimum_vn, _maximum_vn
