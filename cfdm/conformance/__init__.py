541ac68d102d87bbd11a2d4ac5734df10d66d043
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2026-01-21 17:14:07 +0000

    Update testing to new locations of standard name processing

--- cfdm/conformance/__init__.py
+++ cfdm/conformance/__init__.py
@@ -1,5 +1,11 @@
 from .checker import Checker
-from .standardnames import get_all_current_standard_names
+
+from .standardnames import (
+    get_all_current_standard_names,
+    # Intended for internal use but used in testing, so need exposing:
+    _extract_names_from_xml,
+    _STD_NAME_CURRENT_XML_URL
+)
 
 # Eventually structure as with individual modules per class, like:
 # from .domainvariable import DomainVariable
