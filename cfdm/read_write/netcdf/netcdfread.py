5fa3a20a10aaad25c70c53c4cf58caab89888f67
David Hassell <davidhassell@users.noreply.github.com>
2026-02-26 14:54:45 +0000

    Update cfdm/read_write/netcdf/netcdfread.py
    Co-authored-by: Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>

--- cfdm/read_write/netcdf/netcdfread.py
+++ cfdm/read_write/netcdf/netcdfread.py
@@ -8347,9 +8347,6 @@ class NetCDFRead(IORead):
                     )
                     array = np.ma.masked_values(array, "")
 
-            case _:
-                pass
-
         # Set the dask chunking strategy
         chunks = self._dask_chunks(
             array, ncvar, compressed, construct_type=construct_type
